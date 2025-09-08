afb33afb05d903406ac4a39677f72e66fa0332de
Le0nsb <ipb23.l.bluks@vtdt.edu.lv>
2025-09-08 15:11:19 +0300

    Ieliku kreisās daļas skeletu

--- index.php
+++ index.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <p>fdjbhfd</p>
+    <div class="left-container">
+        <div class="full-body">
+            <h2>Full body</h2>
+        </div>
+
+        <div class="upper-body">
+            <h2>Upper body</h2>
+        </div>
+
+        <div class="lower-body">
+            <h2>Lower body</h2>
+        </div>
+    </div>
 </body>
 </html>
